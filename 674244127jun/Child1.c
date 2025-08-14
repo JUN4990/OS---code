6c1867eccbe2f30633b139f2ac4cc50c35034c64
JUN4990 <674244127@mail.pbru.ac.th>
2025-08-14 12:03:41 +0700

    Add files via upload

---
+++ 674244127jun/Child1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main() {
+    printf("My bio.: \n");
+    printf("ID : 674244127 \n");
+    printf("Name : Sittiparthan Boonyong\n");
+    printf("------------------------------\n");
+    
+    
+}
+
